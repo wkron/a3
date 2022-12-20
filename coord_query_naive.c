b4a2da0984c54c42302d64b0f54aaeb6e2cee118
Mark - MakkerW <welinmark@gmail.com>
2022-12-20 15:40:00 +0100

    Coord solution

--- coord_query_naive.c
+++ coord_query_naive.c
@@ -24,17 +24,27 @@ struct naive_data* mk_naive(struct record* rs, int n) {
   naive_data->n = n;
   return naive_data;
   assert(0);
-  // TODO
 }
 
 void free_naive(struct naive_data* data) {
+  free_records(data->rs, data->n);
+  free(data);
   assert(0);
-  // TODO
 }
 
 const struct record* lookup_naive(struct naive_data *data, double lon, double lat) {
+  int n = data->n;
+  struct record* records = data->rs;
+  int shortestindex = 0;
+  double shortestdist = INFINITY;
+  for (int i = 0; i<n; i++){
+    if(euclidean_distance(records[i].lon,records[i].lat,lon,lat)<shortestdist){
+      shortestindex= i;
+      shortestdist=euclidean_distance(records[i].lon,records[i].lat,lon,lat);
+    }
+  }
+  return &records[shortestindex];
   assert(0);
-  // TODO
 }
 
 int main(int argc, char** argv) {
